ba963d814f52f3017f5d66fac36dd43f6be1cf33
Cielorod <skylbr2712@gmail.com>
2026-06-04 16:57:30 -0500

    ya todo

--- android/app/src/main/kotlin/com/example/ejercicios_navegacionn/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ejercicios_navegacionn
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ app_aprendices/android/app/src/main/kotlin/com/example/app_aprendices/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_aprendices
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
